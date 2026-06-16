2a9d029af7e33959db52a1662a52dfda5a947351
Kellogs51 <keremcosgun51@hotmail.com>
2026-06-16 11:24:30 +0200

    logica simple calc

--- calclogic.py
+++ calclogic.py
@@ -1,1 +1,13 @@
-print("hello world")
+def optellen(a, b):
+    return a + b
+
+def aftrekken(a, b):
+    return a - b
+
+def vermenigvuldigen(a, b):
+    return a * b
+
+def delen(a, b):
+    if b == 0:
+        return ("Fout: je kunt niet door 0 delen")
+    return a / b
